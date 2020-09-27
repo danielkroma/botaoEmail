9eb4692b5e5affa4fec1ceff19858f228c0e5a4d
Daniel <danielkroma9@gmail.com>
2020-09-27 16:47:47 -0300

    email

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmailComposer } from '@ionic-native/email-composer/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-
+  constructor(public emailComposer:EmailComposer) {}
+  enviarEmail(){
+    let email = {
+      to: 'danielkroma9@gmail.com',
+      cc: 'danielkroma9@gmail.com',
+      bcc: ['danielkroma9@gmail.com', 'danielkroma9@gmail.com@doe.com'],
+      attachments: [
+        //'file://img/logo.png',
+        //'res://icon.png',
+        //'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
+        //'file://README.pdf'
+      ],
+      subject: 'Cordova Icons',
+      body: 'How are you? Nice greetings from Leipzig',
+      isHtml: true
+     }
+     
+     // Send a text message using default options
+     this.emailComposer.open(email);
+  }
 }
